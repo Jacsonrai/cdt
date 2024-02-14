d1d8598876e8f070178e6b57d4bc07bbde1dd2f1
jacsonrai <jacksonrai1996@gmail.com>
2024-02-15 00:49:18 +0545

    repo initiated

---
+++ src/component/counttimer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const counttimer = () => {
+  return <div>counttimer</div>;
+};
+
+export default counttimer;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className=" text-center text-red-400">Countdown Timer</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
